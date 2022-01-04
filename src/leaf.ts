5630dcd70d5f5c744d5689784137c1771ebe3d79
lwnmengjing <991154416@qq.com>
2022-01-04 17:42:14 +0800

    test

---
+++ src/leaf.ts
@@ -0,0 +1,18 @@
+export interface dict {
+    [key: string]:dict
+}
+
+export function getInclude(all: dict, node: string) :string[] {
+    let include: string[] = [];
+    for (const key in all[node]) {
+        if (Object.prototype.hasOwnProperty.call(all, key)) {
+            const element = all[key];
+            if (Object.keys(element).length == 0) {
+                include.push(key);
+                continue
+            }
+            include.push(...getInclude(all, key))
+        }
+    }
+    return include
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,74 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import { statSync, readdirSync, existsSync, readFileSync } from 'fs'
+import { join } from 'path'
+import { dict, getInclude } from './leaf'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const changePaths: string = core.getInput('change-paths')
+    core.debug(`Chanages ${changePaths} `)
+    const ignorePaths: string = core.getInput('ignore-paths')
+    core.debug(`Ignores ${ignorePaths} `)
+    const workspace: string = core.getInput('workspace')
+    core.debug(`Workspace ${workspace}`)
+    const ignores = ignorePaths.split(',');
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    let dirs:string[] = [];
+    readdirSync(workspace).forEach(e => {
+      if (statSync(join(workspace, e)).isDirectory() && !ignores.includes(e)) {
+        dirs.push(e);
+      }
+    });
 
-    core.setOutput('time', new Date().toTimeString())
+    const depsAll:dict = {};
+    dirs.forEach(p => {
+      depsAll[p] = {};
+      for (const pwd of dirs) {
+        if (pwd === p) {
+          continue
+        }
+        const settingsGradle: string = join(workspace, pwd, 'settings.gradle');
+        if (!existsSync(settingsGradle)) {
+          continue
+        }
+        let lter = readFileSync(settingsGradle, {encoding: 'utf8'}).matchAll(/includeBuild\s\'([^\']+)\'/g)
+        while (!lter.next().done) {
+           const pathInclude = lter.next().value[1]
+           if (pathInclude instanceof String) {
+             if (pathInclude.split('/').length > 0 && pathInclude.split('/')[1] === pwd) {
+               depsAll[pwd][p] = {};
+             }
+           }
+        }
+      }
+    });
+
+    let leaf: string[] = [];
+    let includeNodes: string[] = [];
+
+    changePaths.split(',').forEach(p => {
+      const a = p.split('/')[0].trim()
+      if (ignores.includes(a)) {
+        return
+      }
+      if (statSync(join(workspace, a)).isDirectory()) {
+        if (includeNodes.includes(a)) {
+          return
+        }
+        if (Object.keys(depsAll[a]).length == 0) {
+          leaf.push(a)
+        } else {
+          leaf.push(...getInclude(depsAll, a))
+        }
+      }
+    });
+    leaf = Array.from(new Set(leaf));
+
+    core.setOutput('need_ci', leaf.length > 0);
+    core.setOutput('leaf', leaf);
+    
+
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
